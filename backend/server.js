e3e8ab18df769a9c35451605bea490eafb1c1ff8
Terrance Li <terranceli.terbear@gmail.com>
2018-06-23 22:45:48 -0700

    Added sessions middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 'use strict';
 
+// dependencies
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
 const bodyParser = require('body-parser');
 const routes = require('./routes.js');
 const cors = require('cors');
@@ -9,6 +12,7 @@ const schemas = require('./schemas');
 const app = express();
 const port = 3000;
 
+// mongodb
 mongoose.Promise = global.Promise;
 var mathclubDBConnection = mongoose.connect('mongodb://localhost/mathclubDB');
 var db = mongoose.connection;
@@ -17,9 +21,36 @@ db.once('open', () => {
     console.log('Connected to MongoDB at mongodb://localhost/mathclubDB');
 });
 
+// sessions
+const cookieExpire = 1000 * 60 * 60 * 24 * 7; // 1 week
+app.set('trust proxy', 1);
+
+const sessionMiddleware = session({
+    name: 'session_id',
+    secret: 'QFtjryqLg5I1SuS5dHZ9kkWskZC38AW8SgmPOE1i8Bdk1j3ynTJ9xryg350TyE5U',
+    resave: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: {
+        httpOnly: true,
+        maxAge: cookieExpire,
+        path: '/',
+        secure: false
+    },
+    rolling: true,
+    unset: 'destory'
+});
+
+app.use(sessionMiddleware);
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://165.227.54.2");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 routes(app);
 
 app.listen(port, () => {
