7407f5b7b44479ac0b58e7b634345d0eea2cf27e
Terrance Li <terranceli.terbear@gmail.com>
2018-06-24 10:59:27 -0700

    im being dumb

--- backend/routes.js
+++ backend/routes.js
@@ -11,37 +11,37 @@ module.exports = (app) => {
 
     // member routes
     app.route('/api/members/login/').post(nmcMiddleware.authenticateMember).post(nmcController.login);
-    app.route('/api/members/logout/').get(nmcMiddleware.verifySession).post(nmcController.logout);
+    app.route('/api/members/logout/').post(nmcMiddleware.verifySession).post(nmcController.logout);
     app.route('/api/members/profile/').get(nmcMiddleware.verifySession).get(nmcController.fetchProfile);
-    app.route('/api/member/change-password/').get(nmcMiddleware.verifySession).post(nmcMiddleware.authenticateMember).post(nmcController.changePassword);
+    app.route('/api/member/change-password/').post(nmcMiddleware.verifySession).post(nmcMiddleware.authenticateMember).post(nmcController.changePassword);
 
     // admin math club routes
-    app.route('/api/meetings/add/').get(nmcMiddleware.verifyAdminSession).post(nmcController.newMeeting);
+    app.route('/api/meetings/add/').post(nmcMiddleware.verifyAdminSession).post(nmcController.newMeeting);
     app.route('/api/meetings/').get(nmcMiddleware.verifyAdminSession).get(nmcController.fetchMeetings);
     app.route('/api/members/').get(nmcMiddleware.verifyAdminSession).get(nmcController.fetchMembers);
-    app.route('/api/members/add/').get(nmcMiddleware.verifyAdminSession).post(nmcController.newMember);
-    app.route('/api/members/remove/').get(nmcMiddleware.verifyAdminSession).post(nmcController.removeMember);
-    app.route('/api/members/update/').get(nmcMiddleware.verifyAdminSession).post(nmcController.editMember);
-    app.route('/api/members/promote/').get(nmcMiddleware.verifyAdminSession).post(nmcController.promoteMember);
-    app.route('/api/members/demote/').get(nmcMiddleware.verifyAdminSession).post(nmcController.demoteMember);
+    app.route('/api/members/add/').post(nmcMiddleware.verifyAdminSession).post(nmcController.newMember);
+    app.route('/api/members/remove/').post(nmcMiddleware.verifyAdminSession).post(nmcController.removeMember);
+    app.route('/api/members/update/').post(nmcMiddleware.verifyAdminSession).post(nmcController.editMember);
+    app.route('/api/members/promote/').post(nmcMiddleware.verifyAdminSession).post(nmcController.promoteMember);
+    app.route('/api/members/demote/').post(nmcMiddleware.verifyAdminSession).post(nmcController.demoteMember);
 
     // kpmt school/coach routes
     app.route('/api/kpmt/login/').post(nmcMiddleware.authenticateCoach).post(nmcController.login);
-    app.route('/api/kpmt/logout/').get(nmcMiddleware.verifySession).post(nmcController.logout);
+    app.route('/api/kpmt/logout/').post(nmcMiddleware.verifySession).post(nmcController.logout);
     app.route('/api/kpmt/profile/').get(nmcMiddleware.verifySession).get(nmcController.fetchSchoolProfile);
 
-    app.route('/api/kpmt/team/add/').get(nmcMiddleware.verifySession).post(nmcController.addTeam);
-    app.route('/api/kpmt/team/update/').get(nmcMiddleware.verifySession).post(nmcController.editTeam);
-    app.route('/api/kpmt/team/remove/').get(nmcMiddleware.verifySession).post(nmcController.removeTeam);
+    app.route('/api/kpmt/team/add/').post(nmcMiddleware.verifySession).post(nmcController.addTeam);
+    app.route('/api/kpmt/team/update/').post(nmcMiddleware.verifySession).post(nmcController.editTeam);
+    app.route('/api/kpmt/team/remove/').post(nmcMiddleware.verifySession).post(nmcController.removeTeam);
 
-    app.route('/api/kpmt/indiv/add/').get(nmcMiddleware.verifySession).post(nmcController.addIndiv);
-    app.route('/api/kpmt/indiv/update/').get(nmcMiddleware.verifySession).post(nmcController.editIndiv);
-    app.route('/api/kpmt/indiv/remove/').get(nmcMiddleware.verifySession).post(nmcController.removeIndiv);
+    app.route('/api/kpmt/indiv/add/').post(nmcMiddleware.verifySession).post(nmcController.addIndiv);
+    app.route('/api/kpmt/indiv/update/').post(nmcMiddleware.verifySession).post(nmcController.editIndiv);
+    app.route('/api/kpmt/indiv/remove/').post(nmcMiddleware.verifySession).post(nmcController.removeIndiv);
 
     // kpmt admin management routes
     app.route('/api/kpmt/export/').get(nmcMiddleware.verifyAdminSession).get(nmcController.exportKPMT);
-    app.route('/api/kpmt/clear/').get(nmcMiddleware.verifyAdminSession).post(nmcController.clearKPMT);
-    app.route('/api/kpmt/import/').get(nmcMiddleware.verifyAdminSession).post(nmcController.importKPMT);
+    app.route('/api/kpmt/clear/').post(nmcMiddleware.verifyAdminSession).post(nmcController.clearKPMT);
+    app.route('/api/kpmt/import/').post(nmcMiddleware.verifyAdminSession).post(nmcController.importKPMT);
     app.route('/api/kpmt/validate/').get(nmcMiddleware.verifyAdminSession).get(nmcController.validateKPMT);
 
     app.route('/api/kpmt/competitors/:category').get(nmcMiddleware.verifyAdminSession).get(nmcController.fetchCompetitors);
@@ -49,8 +49,8 @@ module.exports = (app) => {
     app.route('/api/kpmt/schools/:category').get(nmcMiddleware.verifyAdminSession).get(nmcController.fetchSchools);
 
     // kpmt admin scoring routes
-    app.route('/api/kpmt/score/indiv/').get(nmcMiddleware.verifyAdminSession).post(nmcController.scoreIndividual);
-    app.route('/api/kpmt/score/block/').get(nmcMiddleware.verifyAdminSession).post(nmcController.scoreBlock);
-    app.route('/api/kpmt/score/mental/').get(nmcMiddleware.verifyAdminSession).post(nmcController.scoreMentalMath);
-    app.route('/api/kpmt/score/team/:type').get(nmcMiddleware.verifyAdminSession).post(nmcController.scoreTeam);
+    app.route('/api/kpmt/score/indiv/').post(nmcMiddleware.verifyAdminSession).post(nmcController.scoreIndividual);
+    app.route('/api/kpmt/score/block/').post(nmcMiddleware.verifyAdminSession).post(nmcController.scoreBlock);
+    app.route('/api/kpmt/score/mental/').post(nmcMiddleware.verifyAdminSession).post(nmcController.scoreMentalMath);
+    app.route('/api/kpmt/score/team/:type').post(nmcMiddleware.verifyAdminSession).post(nmcController.scoreTeam);
 }
