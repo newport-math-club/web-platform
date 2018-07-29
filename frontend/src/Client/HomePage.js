209bbdffcad9e6f1066b5109359c3c7fa8d73f4f
Terrance Li <terranceli.terbear@gmail.com>
2018-07-29 01:10:20 -0700

    fixed a couple of memes

--- frontend/src/Client/HomePage.js
+++ frontend/src/Client/HomePage.js
@@ -6,7 +6,9 @@ export default class HomePage extends Component {
 		return (
 			<div className="fullheight">
 				<Nav admin={false} items={getNavItems(-1)} />
-				<div className="fill-nav-remainder homepage">
+				<div
+					className="fill-nav-remainder homepage"
+					style={{ width: 'calc(100% - 8em)' }}>
 					<div>
 						<h1>Fridays 3:35-4:30</h1>
 						<h5>Room 1106</h5>
