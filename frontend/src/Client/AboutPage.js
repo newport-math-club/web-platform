2c2ef09d3025481f7562027bddca7e74a63280ab
Terrance Li <terranceli.terbear@gmail.com>
2018-09-11 22:23:18 -0700

    Added Alex's picture

--- frontend/src/Client/AboutPage.js
+++ frontend/src/Client/AboutPage.js
@@ -66,7 +66,7 @@ export default class AboutPage extends Component {
 					<Bio
 						name="Axel Li"
 						title="Shadow Officer"
-						image="https://newport-math-club.nyc3.digitaloceanspaces.com/officers/axel.png"
+						image="https://newport-math-club.nyc3.digitaloceanspaces.com/officers/alex.png"
 						row={9}
 						column={1}
 					/>
