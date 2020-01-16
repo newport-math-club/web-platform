f79db2554efc34b5c03413e28f225c0e9007eb81
Nathan Yan <s-yann@bsd405.org>
2020-01-15 20:55:26 -0800

    Reverted testing code

--- mobile/src/nmc-api.js
+++ mobile/src/nmc-api.js
@@ -1,5 +1,5 @@
-//const BASE_URL = 'https://api.newportmathclub.org'
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = 'https://api.newportmathclub.org'
+//const BASE_URL = "http://localhost:3000"
 
 exports.BASE_URL = BASE_URL
 
