78a195d5bcf378107f32cb77c02cecc553187655
Nathan Yan <s-yann@bsd405.org>
2020-03-05 22:40:24 -0800

    Added covid-19 update

--- frontend/src/Client/HomePage.js
+++ frontend/src/Client/HomePage.js
@@ -9,14 +9,15 @@ export default class HomePage extends Component {
 				<div
 					className="fill-nav-remainder homepage"
 					style={{ width: 'calc(100% - 8em)' }}>
-					<div>
+					<div style = {{width: "50%"}}>
 						<h1>Fridays 3:35-4:30</h1>
 						<h5>Room 1106</h5>
 						<h5>contact@newportmathclub.org</h5>
 						<br />
 						<br />
 						<a href="/kpmt">
-							<h4>Knights of Pi Math Tournament: March 7th</h4>
+							<h4>Due to COVID-19, KPMT is cancelled until further notice.</h4>
+							<h5 style = {{color: "#527aff"}}>If we decide to reschedule KPMT to a later date, registered coaches will be notified through email. Please email if you have questions.</h5>
 						</a>
 					</div>
 				</div>
