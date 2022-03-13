36ace1784cf003a814dd5980da613a8a687bca28
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-03-12 21:30:28 -0800

    Start work on new website
    Hopefully, this will end up more lightweight than  the current website.

---
+++ new/navbar.js
@@ -0,0 +1,22 @@
+let links = {
+	"Home": "index.html"
+};
+class NavBar extends HTMLUListElement {
+	constructor () {
+		super();
+		this.id = "navbar";
+		let stylesheet = document.createElement("link");
+		stylesheet.rel = "stylesheet";
+		stylesheet.href = "navbar.css";
+		this.append(stylesheet);
+		for (const [title, path] of Object.entries(links)) {
+			let linkContainer = document.createElement("li");
+			let link = document.createElement("a");
+			link.href = path;
+			link.textContent = title;
+			linkContainer.append(link);
+			this.append(linkContainer);
+		}
+	}
+}
+customElements.define("nav-bar", NavBar, {extends: "ul"});
