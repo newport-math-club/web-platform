37628dc8e2fa962f199cbd439ad66e6694f090cd
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-04-10 17:56:56 -0700

    Add static site

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = process.env.PORT ?? 3001;
 app.get("/api/ping", (_, res) => {
 	res.send("Pong!");
 });
+app.use(express.static("../frontend"));
 app.listen(port, () => console.log(`Backend is live on port ${port}`));
