74bd3b801896ea3f8c855f63d9dc9176af2b6b89
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-05-01 01:42:34 -0700

    Move backend to Firebase Functions
    Hopefully this doesn't backfire and cost this club $1,000

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+// Dependencies
+const functions = require("firebase-functions");
 const express = require("express");
+// Real stuff
 const app = express();
-const port = process.env.PORT ?? 3001;
 app.get("/api/ping", (_, res) => {
 	res.send("Pong!");
 });
@@ -8,5 +10,4 @@ app.use("/api/register", express.urlencoded({extended: false}));
 app.post("/api/register", (req, res) => {
 	res.send(req.body);
 });
-app.use(express.static("../frontend"));
-app.listen(port, () => console.log(`Backend is live on port ${port}`));
+exports.api = functions.https.onRequest(app);
