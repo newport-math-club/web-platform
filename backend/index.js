221c2c2c1977663b1d544f6222e1981e248e720d
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-09-21 03:05:27 -0700

    Add Firebase thing
    Very barebones thing that I had for a few months when I stopped working on this

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 // Dependencies
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
 const functions = require("firebase-functions");
 const express = require("express");
 // Real stuff
+initializeApp();
+const db = getFirestore();
 const app = express();
 app.get("/api/ping", (_, res) => {
 	res.send("Pong!");
 });
 app.use("/api/register", express.urlencoded({extended: false}));
-app.post("/api/register", (req, res) => {
+app.post("/api/register", async (req, res) => {
+	console.log(req.body);
+	const teamReference = db.collection("teams").doc(req.body["team-name"]);
+	teamReference.set(req.body);
 	res.send(req.body);
 });
 exports.api = functions.https.onRequest(app);
